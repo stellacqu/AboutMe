f11df097accb33f51a9c6d55e157b207eaf75756
Stella <stellaqu04@gmail.com>
2023-07-27 09:31:49 -0400

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by scholar on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
